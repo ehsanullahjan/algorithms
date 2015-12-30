2cbbbf17cacece6ca88ec066a6960bcce92fd84e
Ehsanullah Jan <ehsanullahjan@gmail.com>
2015-12-29 20:49:12 -0500

    Implemented insertion sort algorithm

---
+++ src/main/java/edu/mit/ita/ch02/SortMethods.java
@@ -0,0 +1,34 @@
+package edu.mit.ita.ch02;
+
+public class SortMethods {
+    private SortMethods() {
+    }
+
+    public static <T extends Comparable<T>> void insertionSort(T[] seq) {
+        // Trivially sorted
+        if (seq.length <= 1) {
+            return;
+        }
+
+        // Loop invariant: seq[0..j-1] is always sorted
+        for (int i = 1; i < seq.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (seq[j].compareTo(seq[j - 1]) < 0) {
+                    swap(seq, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static <T extends Comparable<T>> void swap(T[] seq, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        T x = seq[i];
+        seq[i] = seq[j];
+        seq[j] = x;
+    }
+}
---
+++ src/test/groovy/edu/mit/ita/ch02/SortMethodsSpec.groovy
@@ -0,0 +1,25 @@
+package edu.mit.ita.ch02
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static SortMethods.insertionSort
+
+class SortMethodsSpec extends Specification {
+    private static final Integer[] unorderedSeq = [7, 10, 5, 3, 8, 4, 2, 9, 6];
+    private static final Integer[] orderedSeq = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    @Unroll
+    @SuppressWarnings("GroovyAssignabilityCheck")
+    def "Items can be sorted using #method"() {
+        when: "#method is applied to unsorted items"
+        sort(items)
+
+        then: "the items are sorted in place"
+        items == result
+
+        where:
+        method           | sort                    | items                || result
+        "insertion sort" | {a -> insertionSort(a)} | unorderedSeq.clone() || orderedSeq
+    }
+}
