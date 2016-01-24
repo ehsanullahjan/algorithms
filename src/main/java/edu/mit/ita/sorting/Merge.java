d90c4a05f515d8407bee319e00eebb0ab7940274
Ehsanullah Jan <ehsanullahjan@gmail.com>
2016-01-23 23:26:43 -0500

    Refactored merge sort implementation (validation and improved readability)

--- src/main/java/edu/mit/ita/sorting/Merge.java
+++ src/main/java/edu/mit/ita/sorting/Merge.java
@@ -1,42 +1,50 @@
 package edu.mit.ita.sorting;
 
+import static edu.mit.ita.util.ComparableUtil.lt;
+
 public class Merge {
     private Merge() {
     }
 
     @SuppressWarnings("unchecked")
-    public static <T extends Comparable<T>> void sort(T[] a) {
-        sort(a, (T[])new Comparable[a.length], 0, a.length - 1);
+    public static <T extends Comparable<T>> void sort(T[] seq) {
+        if (seq == null || seq.length <= 1) {
+            return;
+        }
+
+        T[] auxSeq = (T[])new Comparable[seq.length];
+        sort(seq, auxSeq, 0, seq.length - 1);
     }
 
-    private static <T extends Comparable<T>> void sort(T[] a, T[] b, int lo, int hi) {
+    private static <T extends Comparable<T>> void sort(T[] seq, T[] auxSeq, int lo, int hi) {
         if (lo >= hi) {
             return;
         }
 
         int mid = (lo + hi) / 2;
-        sort(a, b, lo, mid);
-        sort(a, b, mid + 1, hi);
-        merge(a, b, lo, mid, hi);
+        sort(seq, auxSeq, lo, mid);
+        sort(seq, auxSeq, mid + 1, hi);
+        merge(seq, auxSeq, lo, mid, hi);
     }
 
+    // Precondition: seq[lo..mid] and seq[mid+1..hi] must be sorted
     @SuppressWarnings("ManualArrayCopy")
-    private static <T extends Comparable<T>> void merge(T[] a, T[] b, int lo, int mid, int hi) {
+    private static <T extends Comparable<T>> void merge(T[] seq, T[] auxSeq, int lo, int mid, int hi) {
         for (int i = lo; i <= hi; i++) {
-            b[i] = a[i];
+            auxSeq[i] = seq[i];
         }
 
         int i = lo;
         int j = mid + 1;
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
-                a[k] = b[j++];
+                seq[k] = auxSeq[j++];
             } else if (j > hi) {
-                a[k] = b[i++];
-            } else if (b[i].compareTo(b[j]) < 0) {
-                a[k] = b[i++];
+                seq[k] = auxSeq[i++];
+            } else if (lt(auxSeq[i], auxSeq[j])) {
+                seq[k] = auxSeq[i++];
             } else {
-                a[k] = b[j++];
+                seq[k] = auxSeq[j++];
             }
         }
     }
